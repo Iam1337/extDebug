48ba935255c8e6edefc0130614d724296f8417c1
Vladimir Sigalkin <ext@iron-wall.org>
2021-08-19 21:12:32 +0300

    chore(extDebug): added DMFloat and DMValue classes

---
+++ Assets/extDebug/Scripts/DebugMenu/DMFloat.cs
@@ -0,0 +1,58 @@
+/* Copyright (c) 2021 dr. ext (Vladimir Sigalkin) */
+
+using UnityEngine;
+
+using System;
+
+namespace extDebug
+{
+	public class DMFloat : DMValue<float>
+	{
+		#region Public Vars
+
+		private static readonly string[] _formats = new []{ "0", "0.0", "0.00", "0.000", "0.0000", "0.00000", "0.000000", "0.0000000", "0.00000000" };
+
+		#endregion
+
+		#region Public Vars
+
+		public string Format;
+
+		public int Step;
+
+		#endregion
+
+		#region Private Vars
+
+		private int _precision;
+
+		private int _floatPointScale;
+
+		#endregion
+
+		#region Public Methods
+
+		public DMFloat(DMBranch parent, string path, Func<float> getter, Action<float> setter = null, int order = 0) : base(parent, path, getter, setter, order)
+		{
+			SetPrecision(1);
+		}
+
+		public void SetPrecision(int value)
+		{
+			_precision = Mathf.Clamp(value, 0, _formats.Length - 1);
+			_floatPointScale = (int)Mathf.Pow(10, _precision);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		protected override string ValueToString(float value) => value.ToString(string.IsNullOrEmpty(Format) ? _formats[_precision] : Format );
+
+		protected override float ValueIncrement(float value) => (Mathf.Floor(value * _floatPointScale + 0.1f) + Step) / _floatPointScale;
+
+		protected override float ValueDecrement(float value) => (Mathf.Floor(value * _floatPointScale + 0.1f) - Step) / _floatPointScale;
+
+		#endregion
+	}
+}
