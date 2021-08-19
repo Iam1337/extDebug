d25e5e6169b5fdbef79fe293742f6c421c61097e
Vladimir Sigalkin <ext@iron-wall.org>
2021-08-19 21:12:32 +0300

    chore(extDebug): added DMFloat and DMValue classes

--- Scripts/DebugMenu/DMInteger.cs
+++ Scripts/DebugMenu/DMInteger.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace extDebug
 {
-	public class DMInteger : DMItem
+	public class DMInteger : DMValue<int>
 	{
 		#region Public Vars
 
@@ -14,83 +14,20 @@ namespace extDebug
 
 		#endregion
 
-		#region Private Vars
-
-		private readonly Func<int> _getter;
-
-		private readonly Action<int> _setter;
-
-		private int _defaultValue;
-
-		#endregion
-
 		#region Public Methods
 
-		public DMInteger(DMBranch parent, string path, Func<int> getter, Action<int> setter = null, int order = 0) : base(parent, path, "", order)
-		{
-			_getter = getter;
-			_setter = setter;
-
-			_defaultValue = getter.Invoke();
-			_value = _defaultValue.ToString(Format);
-		}
+		public DMInteger(DMBranch parent, string path, Func<int> getter, Action<int> setter = null, int order = 0) : base(parent, path, getter, setter, order)
+		{ }
 
 		#endregion
 
 		#region Protected Methods
 
-		protected override void OnEvent(EventArgs eventArgs)
-		{
-			if (eventArgs.Event == EventType.Repaint)
-			{
-				var value = _getter.Invoke();
+		protected override string ValueToString(int value) => value.ToString(Format);
 
-				_value = value.ToString(Format);
-				_valueColor = value == _defaultValue ? DM.Colors.Value : DM.Colors.ValueFlash;
-			}
-			else if (eventArgs.Event == EventType.KeyDown)
-			{
-				switch (eventArgs.Key)
-				{
-					case KeyType.Up:
-					{
-						break;
-					}
-					case KeyType.Down:
-					{
-						break;
-					}
-					case KeyType.Left:
-					{
-						if (_setter == null)
-							break;
+		protected override int ValueIncrement(int value) => value + Step;
 
-						_setter.Invoke(_getter.Invoke() - Step);
-
-						break;
-					}
-					case KeyType.Right:
-					{
-						if (_setter == null)
-							break;
-
-						_setter.Invoke(_getter.Invoke() + Step);
-
-						break;
-					}
-					case KeyType.Reset:
-						if (_setter == null)
-							break;
-
-						_setter.Invoke(_defaultValue);
-
-						break;
-					case KeyType.Back:
-						DM.Back();
-						break;
-				}
-			}
-		}
+		protected override int ValueDecrement(int value) => value - Step;
 
 		#endregion
 	}
---
+++ Scripts/DebugMenu/DMValue.cs
@@ -0,0 +1,78 @@
+﻿/* Copyright (c) 2021 dr. ext (Vladimir Sigalkin) */
+
+using System;
+
+namespace extDebug
+{
+    public abstract class DMValue<T> : DMItem
+    {
+        #region Private Vars
+
+        private readonly Func<T> _getter;
+
+        private readonly Action<T> _setter;
+
+        private T _defaultValue;
+
+        #endregion
+
+        #region Public Methods
+
+        protected DMValue(DMBranch parent, string path, Func<T> getter, Action<T> setter = null, int order = 0) : base(parent, path, string.Empty, order)
+        {
+	        _getter = getter;
+	        _setter = setter;
+
+	        _defaultValue = getter.Invoke();
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void OnEvent(EventArgs eventArgs)
+        {
+			if (eventArgs.Event == EventType.Repaint)
+			{
+				var value = _getter.Invoke();
+
+				_valueColor = _defaultValue.Equals(value) ? DM.Colors.Value : DM.Colors.ValueFlash;
+				_value = ValueToString(value);
+			}
+			else if (eventArgs.Event == EventType.KeyDown)
+			{
+				switch (eventArgs.Key)
+				{
+					case KeyType.Left:
+					{
+						_setter?.Invoke(ValueDecrement(_getter.Invoke()));
+						break;
+					}
+					case KeyType.Right:
+					{
+						_setter?.Invoke(ValueIncrement(_getter.Invoke()));
+						break;
+					}
+					case KeyType.Reset:
+					{
+						_setter?.Invoke(_defaultValue);
+						break;
+					}
+					case KeyType.Back:
+					{
+						DM.Back();
+						break;
+					}
+				}
+			}
+		}
+
+        protected abstract string ValueToString(T value);
+
+        protected abstract T ValueIncrement(T value);
+
+        protected abstract T ValueDecrement(T value);
+
+        #endregion
+    }
+}
