f9c24ca8e0b61b951130d415ef668c214f094132
Vladimir Sigalkin <vladimir@sigalkin.ru>
2021-08-21 04:33:57 +0300

    chore(extDebug): add DMEnum and DMEnumRequest

---
+++ Scripts/DebugMenu/DMEnumRequest.cs
@@ -0,0 +1,23 @@
+﻿/* Copyright (c) 2021 dr. ext (Vladimir Sigalkin) */
+
+using System;
+using System.Collections.Generic;
+
+namespace extDebug
+{
+	public class DMEnumRequest<TEnum, TObject> : DMItemRequest<TEnum, TObject> where TEnum : struct, Enum
+	{
+		#region Public Methods
+
+		public DMEnumRequest(Func<IList<TObject>> request, Func<TEnum> getter, Action<TEnum> setter = null, Func<TObject, string> name = null) : base(request, getter, setter, name)
+		{ }
+
+		#endregion
+
+		#region Protected Methods
+
+		protected override DMItem BuildItem(DMBranch parent, TObject @object, string name, Func<TEnum> getter, Action<TEnum> setter, int order) => DM.Add(parent, name, getter, setter, order);
+
+		#endregion
+	}
+}
