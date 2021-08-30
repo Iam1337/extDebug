be73a2ddd8bb0af23eb78a3b759a76554a687164
Vladimir Sigalkin <ext@iron-wall.org>
2021-08-30 13:24:04 +0300

    chore(extDebug): added enum flags menu

--- Assets/extDebug/Scripts/DebugMenu/DMEnum.cs
+++ Assets/extDebug/Scripts/DebugMenu/DMEnum.cs
@@ -1,14 +1,15 @@
 ﻿/* Copyright (c) 2021 dr. ext (Vladimir Sigalkin) */
 
 using System;
+using UnityEngine;
 
 namespace extDebug
 {
     public class DMEnum<T> : DMValue<T> where T : struct, Enum
     {
-		#region Static Private Methods
+        #region Static Private Methods
 
-		private static T NextEnum<T>(T value) where T : struct
+        private static T NextEnum(T value)
 		{
 			var length = Enum.GetValues(typeof(T)).Length;
 			var index = Convert.ToInt64(value) + 1;
@@ -19,7 +20,7 @@ namespace extDebug
 			return (T) Enum.ToObject(typeof(T), index);
 		}
 
-		private static T PrevEnum<T>(T value) where T : struct
+		private static T PrevEnum(T value)
 		{
 			var length = Enum.GetValues(typeof(T)).Length;
 			var index = Convert.ToInt64(value) - 1;
@@ -32,18 +33,81 @@ namespace extDebug
 
 		#endregion
 
+		#region Private Vars
+
+		private readonly DMBranch _flagBranch;
+
+		#endregion
+
 		#region Public Methods
 
 		public DMEnum(DMBranch parent, string path, Func<T> getter, Action<T> setter = null, int order = 0) : base(parent, path, getter, setter, order)
-		{ }
+		{
+			if (setter != null)
+			{
+				var type = typeof(T);
+				if (type.IsDefined(typeof(FlagsAttribute), false))
+				{
+					_flagBranch = new DMBranch(null, GetPathName(path), getter.Invoke().ToString());
+
+					var i = 0;
+					var values = (T[])Enum.GetValues(type);
+					for (; i < values.Length; i++)
+					{
+						var value = values[i];
+
+						DM.Add(_flagBranch, value.ToString(), () =>
+						{
+							var intGetter = (int)(object)getter.Invoke();
+							var intValue = (int)(object)value;
+
+							return (intGetter & intValue) != 0;
+						}, v =>
+						{
+							var intGetter = (int)(object)getter.Invoke();
+							var intValue = (int)(object)value;
+
+							setter.Invoke((T)(object)(intGetter ^ intValue));
+						}, i);
+					}
+
+					DM.Add(_flagBranch, "Back", _ => DM.Back(), string.Empty, i + 10);
+				}
+			}
+		}
 
 		#endregion
 
 		#region Protected Methods
 
+		protected override void OnEvent(EventTag eventTag)
+		{
+			if (_flagBranch != null && eventTag == EventTag.Left)
+			{
+				DM.Back();
+			}
+			else if (_flagBranch != null && eventTag == EventTag.Right)
+			{
+				DM.Open(_flagBranch);
+			}
+			else
+			{
+				base.OnEvent(eventTag);
+			}
+		}
+
 		protected override string ValueToString(T value)
 		{
-			return null;
+			if (_flagBranch != null)
+			{
+				var intValue = (int)(object)value;
+				if (intValue == 0)
+				{
+					return "None";
+				}
+			}
+
+			return value.ToString();
 		}
 
 		protected override T ValueIncrement(T value) => NextEnum(value);
