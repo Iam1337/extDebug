5a64623da08cf58fd9a5a66c50fa6992ada56cd5
Vladimir Sigalkin <ext@iron-wall.org>
2021-08-31 17:11:34 +0300

    chore(extDebug): start work on heatmap

---
+++ Scripts/Heatmap/DHSession.cs
@@ -0,0 +1,37 @@
+﻿/* Copyright (c) 2021 dr. ext (Vladimir Sigalkin) */
+
+using UnityEngine;
+
+namespace extDebug.Heatmap
+{
+    public class DHSession
+    {
+        #region Public Vars
+
+        #endregion
+
+        #region Public Methods
+
+        public void Point(string tag, Vector3 position, Quaternion rotation)
+        {
+	        
+        }
+
+        public void StartTrack(string tag, Transform transform)
+        {
+	        
+        }
+
+        public void StopTrack(string tag)
+        {
+	        
+        }
+
+        public void Update()
+        {
+
+        }
+
+        #endregion
+    }
+}
