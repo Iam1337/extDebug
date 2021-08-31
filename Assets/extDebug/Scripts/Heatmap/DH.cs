6be7efbe05ec5f6bf6ad21206fa8c65de5947303
Vladimir Sigalkin <ext@iron-wall.org>
2021-08-31 17:11:34 +0300

    chore(extDebug): start work on heatmap

---
+++ Assets/extDebug/Scripts/Heatmap/DH.cs
@@ -0,0 +1,52 @@
+/* Copyright (c) 2021 dr. ext (Vladimir Sigalkin) */
+
+using UnityEngine;
+
+namespace extDebug.Heatmap
+{
+    public static class DH
+    {
+        #region Static Private Vars
+
+        private static DHSession _currentSession;
+
+        #endregion
+
+        #region Static Public Methods
+
+        static DH()
+        {
+            Hooks.Update += Update;
+        }
+
+        public static void StartSession(string sessionName)
+        {
+            // TODO: Create session.
+        }
+
+        public static void StopSession()
+        {
+            // TODO: Stop session.
+
+            _currentSession = null;
+        }
+
+        // Push event
+        public static void Point(string tag, Vector3 position, Quaternion rotation) => _currentSession?.Point(tag, position, rotation);
+
+        public static void StartTrack(string tag, Transform transform) => _currentSession?.StartTrack(tag, transform);
+
+        public static void StopTrack(string tag) => _currentSession?.StopTrack(tag);
+
+        #endregion
+
+        #region Static Private Methods
+
+        private static void Update()
+        {
+	         _currentSession?.Update();
+        }
+
+        #endregion
+    }
+}
